720e15183b9223a2217f2d32d5fc38bdf78bf2df
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-03 22:18:48 +0900

    feat: 設定を変更

--- jest.config.js
+++ jest.config.js
@@ -12,9 +12,12 @@ const customJestConfig = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    // aliasを定義 （tsconfig.jsonのcompilerOptions>pathsの定義に合わせる）
+    '^@/(.*)$': '<rootDir>/$1',
+  },
   // ↓1行を変更しています: React Testing Libraryはまだ利用しないのでコメントアウト
-  // testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jest-environment-jsdom',
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
