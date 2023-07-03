2d05543bcc1e3220d91849bbce08fa2db87d8eab
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-03 13:49:05 +0900

    feat: ワークフローテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Home: NextPage = () => {
+  const [isOpen] = useState();
+
+  useEffect(() => {
+    console.log(isOpen);
+  }, []);
+
   return <div>home</div>;
 };
 
