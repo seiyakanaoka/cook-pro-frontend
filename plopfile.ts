48cd27d8e53135f55b0c43f009a8e00b0268c182
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-04 17:08:29 +0900

    feat: 設定の見直しと、テストファイルのテンプレート作成

--- plopfile.ts
+++ plopfile.ts
@@ -7,16 +7,57 @@ export default function (plop: NodePlopAPI) {
     prompts: [
       {
         type: 'input',
+        name: 'path',
+        message:
+          '以下のコンポーネントのディレクトリパスを入力してください\nsrc/components/以下で入力してください',
+      },
+      {
+        type: 'input',
         name: 'name',
-        message: "What is this component's name?",
+        message: 'コンポーネント名を入力してください',
       },
       {
         type: 'list',
-        name: 'element',
-        message: 'HTML element (div is default)',
+        name: 'type',
+        message: 'コンポーネントの種別を選択してください',
         choices: ['Server Component', 'Client Component'],
       },
     ],
-    actions: [], // array of actions
+    actions: (data) => {
+      const path = `../src/components/{{path}}/{{name}}`;
+
+      const component =
+        data?.type === 'Server Component'
+          ? {
+              type: 'add',
+              path: path + `index.tsx`,
+              templateFile: 'component/ServerComponent.tsx.hbs',
+            }
+          : {
+              type: 'add',
+              path: path + `index.tsx`,
+              templateFile: 'component/ClientComponent.tsx.hbs',
+            };
+
+      const actions = [
+        component,
+        {
+          type: 'add',
+          path: path + `index.ts`,
+          templateFile: 'component/index.ts.hbs',
+        },
+        {
+          type: 'add',
+          path: path + `index.module.scss`,
+          templateFile: 'component/style.ts.hbs',
+        },
+        {
+          type: 'add',
+          path: path + `{{name}}.test.ts`,
+          templateFile: 'BaseComponent/test.ts.hbs',
+        },
+      ];
+      return actions;
+    },
   });
 }
