6e16565a1b6a68601f8fe2a3ec9e53de33166661
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-06 21:30:52 +0900

    feat: ボタンクリックテストを追加

--- src/components/ui/Button/Button.test.tsx
+++ src/components/ui/Button/Button.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { Button } from './Button';
 
@@ -15,5 +16,47 @@ describe('Buttonコンポーネント', () => {
     render(<Button text={text} color="primary" onClick={() => {}} />);
     const element = screen.getByRole('button');
     expect(element).toHaveTextContent(text);
+  });
+
+  test('propsで渡したonClickが一度実行されていること', () => {
+    const onClick = jest.fn();
+    render(<Button text="text" color="primary" onClick={onClick} />);
+    const element = screen.getByRole('button');
+    userEvent.click(element);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('propsで渡したisDisabledがfalseの場合、ボタンは活性化していて、onClickが一度実行されていること', () => {
+    const isDisabled = false;
+    const onClick = jest.fn();
+    render(
+      <Button
+        text="text"
+        color="primary"
+        isDisabled={isDisabled}
+        onClick={onClick}
+      />
+    );
+    const element = screen.getByRole('button');
+    userEvent.click(element);
+    expect(element).toBeEnabled();
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('propsで渡したisDisabledがtrueの場合、ボタンは非活性で、onClickが一度も実行されないこと', () => {
+    const isDisabled = true;
+    const onClick = jest.fn();
+    render(
+      <Button
+        text="text"
+        color="primary"
+        isDisabled={isDisabled}
+        onClick={onClick}
+      />
+    );
+    const element = screen.getByRole('button');
+    userEvent.click(element);
+    expect(element).toBeDisabled();
+    expect(onClick).toHaveBeenCalledTimes(0);
   });
 });
