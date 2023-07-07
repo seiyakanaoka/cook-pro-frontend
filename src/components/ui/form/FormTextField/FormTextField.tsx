b8d27e772a3230e2a2025c52618fd93f0df61ef5
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-07 19:48:21 +0900

    feat: テンプレート作成

---
+++ src/components/ui/form/FormTextField/FormTextField.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { FC } from 'react';
+
+import style from './index.module.scss';
+
+type Props = {}
+
+export const FormTextField: FC<Props> = ({}: Props) => {
+  return (
+    <div className={style['default']}></div>
+  );
+};
