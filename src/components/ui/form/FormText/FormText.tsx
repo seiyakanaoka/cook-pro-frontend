7025686873a834a0ee90efe39aab9196338634d5
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-07 21:51:04 +0900

    feat: マークアップ作成

--- src/components/ui/form/FormText/FormText.tsx
+++ src/components/ui/form/FormText/FormText.tsx
@@ -1,11 +1,29 @@
 'use client';
 
-import { FC } from 'react';
+import { ChangeEventHandler, FC, FocusEventHandler } from 'react';
+
+import { FormTextField } from '../FormTextField';
+import { FormTitle } from '../FormTitle';
 
 import style from './index.module.scss';
 
-type Props = {};
+type Props = {
+  title: string;
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  onBlur?: FocusEventHandler<HTMLInputElement>;
+};
 
-export const FormText: FC<Props> = ({}: Props) => {
-  return <div className={style['form-text-component']}></div>;
+export const FormText: FC<Props> = ({
+  title,
+  value,
+  onChange,
+  onBlur,
+}: Props) => {
+  return (
+    <div className={style['form-text-component']}>
+      <FormTitle title={title} />
+      <FormTextField value={value} onChange={onChange} onBlur={onBlur} />
+    </div>
+  );
 };
