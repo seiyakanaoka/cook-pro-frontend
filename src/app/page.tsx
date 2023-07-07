0af81ff0e948b683f21775b33eadc28ce644c3a9
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-07 19:22:08 +0900

    feat: タイトル表示できるようにする

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next';
 import React from 'react';
 
+import { FormTitle } from '@/components/ui/form/FormTitle';
+
 const Home: NextPage = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      home
+      <FormTitle title="テスト" />
+    </div>
+  );
 };
 
 export default Home;
--- src/components/ui/form/FormTitle/FormTitle.tsx
+++ src/components/ui/form/FormTitle/FormTitle.tsx
@@ -4,8 +4,16 @@ import { FC } from 'react';
 
 import style from './index.module.scss';
 
-type Props = {};
+type Props = {
+  title: string;
+  isRequired?: boolean;
+};
 
-export const FormTitle: FC<Props> = ({}: Props) => {
-  return <div className={style['form-title-component']}></div>;
+export const FormTitle: FC<Props> = ({ title, isRequired }: Props) => {
+  return (
+    <div className={style['form-title-component']}>
+      {title}
+      <div className={style['icon']}></div>
+    </div>
+  );
 };
