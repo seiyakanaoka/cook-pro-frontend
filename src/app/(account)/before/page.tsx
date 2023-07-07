de2b33feb2b74c651d7ab94acc20a28c139afa79
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-07 13:56:17 +0900

    feat: ページを追加

---
+++ src/app/(account)/before/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const SignupLoginBeforePage: NextPage = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default SignupLoginBeforePage;
