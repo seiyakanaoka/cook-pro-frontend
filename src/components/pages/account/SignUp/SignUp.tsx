7290e1c0623defd957ae6e2f25481c30096ce51f
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-07 19:05:39 +0900

    feat: テンプレート作成

---
+++ src/components/pages/account/SignUp/SignUp.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { FC } from 'react';
+
+import style from './index.module.scss';
+
+type Props = {}
+
+export const SignUp: FC<Props> = ({}: Props) => {
+  return (
+    <div className={style['default']}></div>
+  );
+};
