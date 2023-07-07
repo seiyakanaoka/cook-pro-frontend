df1520c4fae764319fc9372a76aa3b3aae109a20
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-07 12:58:44 +0900

    feat: ロゴの追加とsvgrの設定を追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // svgrの設定
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
