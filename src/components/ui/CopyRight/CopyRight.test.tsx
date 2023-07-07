500014f167dc7efaf4faa6b3bd09644b15613cf9
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-07 16:36:41 +0900

    feat: テスト作成

--- src/components/ui/CopyRight/CopyRight.test.tsx
+++ src/components/ui/CopyRight/CopyRight.test.tsx
@@ -1,2 +1,12 @@
-import '@testing-library/jest-dom'; describe("CopyRightコンポーネント", () => {
-test("test", () => {}); });
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import { CopyRight } from './CopyRight';
+
+describe('CopyRightコンポーネント', () => {
+  test('「©️ Cook Pro 2023」という表示であること', () => {
+    render(<CopyRight />);
+    const element = screen.getByText('©️ Cook Pro 2023');
+    expect(element).toBeInTheDocument();
+  });
+});
