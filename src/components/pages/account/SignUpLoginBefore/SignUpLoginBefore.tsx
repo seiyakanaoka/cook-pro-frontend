245f1d7d1e0cea2d2a3a87cf977d68b90d4ecc1f
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-07 16:28:15 +0900

    feat: 画面遷移する関数を作成して、ボタンに渡す

--- src/components/pages/account/SignUpLoginBefore/SignUpLoginBefore.tsx
+++ src/components/pages/account/SignUpLoginBefore/SignUpLoginBefore.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { FC } from 'react';
 
 import { Button } from '@/components/ui/Button';
@@ -8,6 +9,16 @@ import { Logo } from '@/components/ui/Logo';
 import style from './index.module.scss';
 
 export const SignUpLoginBefore: FC = () => {
+  const { push } = useRouter();
+
+  const navigateToLogin = () => {
+    push('/login');
+  };
+
+  const navigateToSignUp = () => {
+    push('/signup');
+  };
+
   return (
     <div className={style['signup-login-before']}>
       <div className={style['top']}>
@@ -17,13 +28,17 @@ export const SignUpLoginBefore: FC = () => {
       </div>
       <div className={style['bottom']}>
         <div className={style['account-actions']}>
-          <Button text="ログイン" color="primary" onClick={() => {}} />
+          <Button text="ログイン" color="primary" onClick={navigateToLogin} />
           <div className={style['guide']}>
             <div className={style['line']} />
             <p className={style['text']}>または</p>
             <div className={style['line']} />
           </div>
-          <Button text="新規登録" color="secondary" onClick={() => {}} />
+          <Button
+            text="新規登録"
+            color="secondary"
+            onClick={navigateToSignUp}
+          />
         </div>
         <p className={style['copyright']}>©️ Cook Pro 2023</p>
       </div>
