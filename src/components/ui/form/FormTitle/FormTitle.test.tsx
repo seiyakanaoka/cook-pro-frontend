f13793aa3067ca956626fa3b9a27fc3341e7dd05
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-09 19:39:08 +0900

    feat: svgファイルをdivに変換する設定を追加変換する設定を追加

--- src/components/ui/form/FormTitle/FormTitle.test.tsx
+++ src/components/ui/form/FormTitle/FormTitle.test.tsx
@@ -1,2 +1,11 @@
-import '@testing-library/jest-dom'; describe("FormTitleコンポーネント", () => {
-test("test", () => {}); });
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+
+import { FormTitle } from './FormTitle';
+
+describe('FormTitleコンポーネント', () => {
+  test('propsで渡したtitleが表示されていること', () => {
+    const title = 'hello world';
+    render(<FormTitle title={title} />);
+  });
+});
