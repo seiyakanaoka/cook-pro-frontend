7315bb008af1e8ff081331399e29a25094764a22
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-09 20:57:23 +0900

    feat: 入力フォームの追加

--- src/components/pages/account/SignUp/SignUp.tsx
+++ src/components/pages/account/SignUp/SignUp.tsx
@@ -1,11 +1,119 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, ChangeEventHandler, useState } from 'react';
+
+import { FormText } from '@/components/ui/form/FormText';
 
 import style from './index.module.scss';
 
 type Props = {};
 
 export const SignUp: FC<Props> = ({}: Props) => {
-  return <div className={style['sign-up']}></div>;
+  const [lastName, setLastName] = useState<string>('');
+
+  const handleChangeLastName: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setLastName(e.currentTarget.value);
+  };
+
+  const [firstName, setFirstName] = useState<string>('');
+
+  const handleChangeFirstName: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFirstName(e.currentTarget.value);
+  };
+
+  const [lastNameKana, setLastNameKana] = useState<string>('');
+
+  const handleChangeLastNameKana: ChangeEventHandler<HTMLInputElement> = (
+    e
+  ) => {
+    setLastNameKana(e.currentTarget.value);
+  };
+
+  const [firstNameKana, setFirstNameKana] = useState<string>('');
+
+  const handleChangeFirstNameKana: ChangeEventHandler<HTMLInputElement> = (
+    e
+  ) => {
+    setFirstNameKana(e.currentTarget.value);
+  };
+
+  const [email, setEmail] = useState<string>('');
+
+  const handleChangeEmail: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const [emailConfirm, setEmailConfirm] = useState<string>('');
+
+  const handleChangeEmailConfirm: ChangeEventHandler<HTMLInputElement> = (
+    e
+  ) => {
+    setEmailConfirm(e.currentTarget.value);
+  };
+
+  const [password, setPassword] = useState<string>('');
+
+  const handleChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('');
+
+  const handleChangePasswordConfirm: ChangeEventHandler<HTMLInputElement> = (
+    e
+  ) => {
+    setPasswordConfirm(e.currentTarget.value);
+  };
+
+  const [telephone, setTelephone] = useState<string>('');
+
+  const handleChangeTelephone: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTelephone(e.currentTarget.value);
+  };
+
+  return (
+    <div className={style['sign-up']}>
+      <div className={style['field']}>
+        <FormText title="姓" value={lastName} onChange={handleChangeLastName} />
+        <FormText
+          title="名"
+          value={firstName}
+          onChange={handleChangeFirstName}
+        />
+      </div>
+      <div className={style['field']}>
+        <FormText
+          title="姓カナ"
+          value={lastNameKana}
+          onChange={handleChangeLastNameKana}
+        />
+        <FormText
+          title="名カナ"
+          value={firstNameKana}
+          onChange={handleChangeFirstNameKana}
+        />
+      </div>
+      <FormText title="Email" value={email} onChange={handleChangeEmail} />
+      <FormText
+        title="Email確認"
+        value={emailConfirm}
+        onChange={handleChangeEmailConfirm}
+      />
+      <FormText
+        title="パスワード"
+        value={password}
+        onChange={handleChangePassword}
+      />
+      <FormText
+        title="パスワード確認"
+        value={passwordConfirm}
+        onChange={handleChangePasswordConfirm}
+      />
+      <FormText
+        title="電話番号"
+        value={telephone}
+        onChange={handleChangeTelephone}
+      />
+    </div>
+  );
 };
