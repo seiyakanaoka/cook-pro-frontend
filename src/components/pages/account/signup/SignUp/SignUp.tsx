730bb7ee9ad487722867cf9ab656fca6e10597a6
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-10 22:29:28 +0900

    feat: バリデーションを設定

--- src/components/pages/account/signup/SignUp/SignUp.tsx
+++ src/components/pages/account/signup/SignUp/SignUp.tsx
@@ -59,53 +59,93 @@ export const SignUp: FC<Props> = ({}: Props) => {
             errorMessage={errors?.lastName}
             onChange={(e) =>
               onChange('lastName', e, {
-                required: { value: true },
-                maxLength: { value: 5, message: '最大数' },
+                required: { value: true, message: '入力してください。' },
+                maxLength: { value: 5, message: '100文字まで入力可能です。' },
               })
             }
           />
           <FormText
             title="名"
             value={fieldValue.firstName}
-            onChange={(e) => onChange('firstName', e)}
+            onChange={(e) =>
+              onChange('firstName', e, {
+                required: { value: true, message: '入力してください。' },
+                maxLength: { value: 5, message: '100文字まで入力可能です。' },
+              })
+            }
           />
         </div>
         <div className={style['field']}>
           <FormText
             title="姓カナ"
             value={fieldValue.lastNameKana}
-            onChange={(e) => onChange('lastNameKana', e)}
+            onChange={(e) =>
+              onChange('lastNameKana', e, {
+                required: { value: true, message: '入力してください。' },
+                maxLength: { value: 5, message: '100文字まで入力可能です。' },
+              })
+            }
           />
           <FormText
             title="名カナ"
             value={fieldValue.firstNameKana}
-            onChange={(e) => onChange('firstNameKana', e)}
+            onChange={(e) =>
+              onChange('firstNameKana', e, {
+                required: { value: true, message: '入力してください。' },
+                maxLength: { value: 5, message: '100文字まで入力可能です。' },
+              })
+            }
           />
         </div>
         <FormText
           title="Email"
           value={fieldValue.email}
-          onChange={(e) => onChange('email', e)}
+          onChange={(e) =>
+            onChange('email', e, {
+              required: { value: true, message: '入力してください。' },
+              maxLength: { value: 5, message: '100文字まで入力可能です。' },
+            })
+          }
         />
         <FormText
           title="Email確認"
           value={fieldValue.emailConfirm}
-          onChange={(e) => onChange('emailConfirm', e)}
+          onChange={(e) =>
+            onChange('emailConfirm', e, {
+              required: { value: true, message: '入力してください。' },
+              maxLength: { value: 5, message: '100文字まで入力可能です。' },
+            })
+          }
         />
         <FormText
           title="パスワード"
           value={fieldValue.password}
-          onChange={(e) => onChange('password', e)}
+          onChange={(e) =>
+            onChange('password', e, {
+              required: { value: true, message: '入力してください。' },
+              maxLength: { value: 5, message: '100文字まで入力可能です。' },
+            })
+          }
         />
         <FormText
           title="パスワード確認"
           value={fieldValue.passwordConfirm}
-          onChange={(e) => onChange('passwordConfirm', e)}
+          onChange={(e) =>
+            onChange('passwordConfirm', e, {
+              required: { value: true, message: '入力してください。' },
+              maxLength: { value: 5, message: '100文字まで入力可能です。' },
+            })
+          }
         />
         <FormText
           title="電話番号"
           value={fieldValue.telephone}
-          onChange={(e) => onChange('telephone', e)}
+          onChange={(e) =>
+            onChange('telephone', e, {
+              required: { value: true, message: '入力してください。' },
+              maxLength: { value: 5, message: '100文字まで入力可能です。' },
+            })
+          }
         />
       </div>
       <div className={style['actions']}>
