dda97ef0a9b9d05ba774aaa2de29db76ab396558
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-10 14:16:46 +0900

    feat: マークアップ作成

--- src/components/pages/account/SignUp/SignUp.tsx
+++ src/components/pages/account/SignUp/SignUp.tsx
@@ -2,6 +2,7 @@
 
 import { FC } from 'react';
 
+import { Button } from '@/components/ui/Button';
 import { FormText } from '@/components/ui/form/FormText';
 import { useFormText } from '@/hooks/useFormText';
 
@@ -36,56 +37,62 @@ export const SignUp: FC<Props> = ({}: Props) => {
 
   return (
     <form className={style['sign-up']} onSubmit={onSubmit}>
-      <div className={style['field']}>
+      <h1 className={style['title']}>新規登録</h1>
+      <div className={style['sign-up-field']}>
+        <div className={style['field']}>
+          <FormText
+            title="姓"
+            value={fieldValue.lastName}
+            onChange={(e) => onChange('lastName', e)}
+          />
+          <FormText
+            title="名"
+            value={fieldValue.firstName}
+            onChange={(e) => onChange('firstName', e)}
+          />
+        </div>
+        <div className={style['field']}>
+          <FormText
+            title="姓カナ"
+            value={fieldValue.lastNameKana}
+            onChange={(e) => onChange('lastNameKana', e)}
+          />
+          <FormText
+            title="名カナ"
+            value={fieldValue.firstNameKana}
+            onChange={(e) => onChange('firstNameKana', e)}
+          />
+        </div>
         <FormText
-          title="姓"
-          value={fieldValue.lastName}
-          onChange={(e) => onChange('lastName', e)}
+          title="Email"
+          value={fieldValue.email}
+          onChange={(e) => onChange('email', e)}
         />
         <FormText
-          title="名"
-          value={fieldValue.firstName}
-          onChange={(e) => onChange('firstName', e)}
+          title="Email確認"
+          value={fieldValue.emailConfirm}
+          onChange={(e) => onChange('emailConfirm', e)}
+        />
+        <FormText
+          title="パスワード"
+          value={fieldValue.password}
+          onChange={(e) => onChange('password', e)}
+        />
+        <FormText
+          title="パスワード確認"
+          value={fieldValue.passwordConfirm}
+          onChange={(e) => onChange('passwordConfirm', e)}
+        />
+        <FormText
+          title="電話番号"
+          value={fieldValue.telephone}
+          onChange={(e) => onChange('telephone', e)}
         />
       </div>
-      <div className={style['field']}>
-        <FormText
-          title="姓カナ"
-          value={fieldValue.lastNameKana}
-          onChange={(e) => onChange('lastNameKana', e)}
-        />
-        <FormText
-          title="名カナ"
-          value={fieldValue.firstNameKana}
-          onChange={(e) => onChange('firstNameKana', e)}
-        />
+      <div className={style['actions']}>
+        <Button text="進む" color="primary" onClick={() => {}} />
+        <Button text="キャンセル" color="secondary" onClick={() => {}} />
       </div>
-      <FormText
-        title="Email"
-        value={fieldValue.email}
-        onChange={(e) => onChange('email', e)}
-      />
-      <FormText
-        title="Email確認"
-        value={fieldValue.emailConfirm}
-        onChange={(e) => onChange('emailConfirm', e)}
-      />
-      <FormText
-        title="パスワード"
-        value={fieldValue.password}
-        onChange={(e) => onChange('password', e)}
-      />
-      <FormText
-        title="パスワード確認"
-        value={fieldValue.passwordConfirm}
-        onChange={(e) => onChange('passwordConfirm', e)}
-      />
-      <FormText
-        title="電話番号"
-        value={fieldValue.telephone}
-        onChange={(e) => onChange('telephone', e)}
-      />
-      <button>ssss</button>
     </form>
   );
 };
