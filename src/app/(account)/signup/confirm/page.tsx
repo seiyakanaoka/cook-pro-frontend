3d7baca77ac6d1268c6a3d553072413800775080
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-16 11:54:26 +0900

    feat: テンプレート作成

--- src/app/(account)/signup/confirm/page.tsx
+++ src/app/(account)/signup/confirm/page.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+import { SignUpConfirm } from '@/components/pages/account/signup/SignUpConfirm';
+
 const SignUpConfirmPage: NextPage = () => {
-  return <></>;
+  return <SignUpConfirm />;
 };
 
 export default SignUpConfirmPage;
