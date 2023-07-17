cea4ac153936c50f5bd50114d92e2f5d0ab806b7
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-17 16:52:04 +0900

    feat: propsを追加

--- src/components/ui/form/FormTextField/FormTextField.tsx
+++ src/components/ui/form/FormTextField/FormTextField.tsx
@@ -2,21 +2,26 @@
 
 import { ChangeEventHandler, FC, FocusEventHandler } from 'react';
 
+import { FORM_TEXT_FIELD_TYPE, FormTextFieldType } from '@/constants/form';
+
 import style from './index.module.scss';
 
 type Props = {
   value: string;
+  type?: FormTextFieldType;
   onChange: ChangeEventHandler<HTMLInputElement>;
   onBlur?: FocusEventHandler<HTMLInputElement>;
 };
 
 export const FormTextField: FC<Props> = ({
   value,
+  type = FORM_TEXT_FIELD_TYPE.TEXT,
   onChange,
   onBlur,
 }: Props) => {
   return (
     <input
+      type={type}
       className={style['form-text-field-component']}
       value={value}
       onChange={onChange}
