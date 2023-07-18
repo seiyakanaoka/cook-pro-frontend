29fa7531d87971c357fdb0b780080c00f0ea4864
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-18 14:50:32 +0900

    feat: htmlとcss作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import type { NextPage } from 'next';
 
-import { DishTime } from '@/components/ui/dish/DishTime';
+import { DishItem } from '@/components/ui/dish/DishItem';
+import Food1Icon from 'public/food-1.png';
 
 const Home: NextPage = () => {
   return (
     <div>
-      home
-      <br />
-      <DishTime time={new Date()} />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '24px',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <DishItem
+          title="季節の野菜たっぷり
+のカレー"
+          image={Food1Icon.src}
+        />
+        <DishItem
+          title="季節の野菜たっぷり
+のカレー"
+          image={Food1Icon.src}
+        />
+      </div>
     </div>
   );
 };
--- src/components/ui/dish/DishItem/DishItem.tsx
+++ src/components/ui/dish/DishItem/DishItem.tsx
@@ -2,6 +2,8 @@
 
 import { FC } from 'react';
 
+import { DishTime } from '../DishTime';
+
 import style from './index.module.scss';
 
 type Props = { image: string; title: string };
@@ -9,8 +11,13 @@ type Props = { image: string; title: string };
 export const DishItem: FC<Props> = ({ image, title }: Props) => {
   return (
     <div className={style['dish-component']}>
-      <img src={image} alt="" />
-      <p>{title}</p>
+      <div className={style['image-field']}>
+        <img src={image} alt="" className={style['image']} />
+        <div className={style['time']}>
+          <DishTime time={new Date()} />
+        </div>
+      </div>
+      <p className={style['title']}>{title}</p>
     </div>
   );
 };
