40ce486279a2fb32da1935eda58152349998ca02
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-19 15:24:59 +0900

    feat: ヘッダーの配置と、placeholderを追加

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -17,9 +17,18 @@ type Props = {
 export const Header: FC<Props> = ({ userImage, value, onChange }: Props) => {
   return (
     <div className={style['header-component']}>
-      <img src={userImage} alt="" className={style['image']} />
-      <FormTextField value={value} onChange={onChange} />
-      <img src={LogoImage.src} alt="" className={style['image']} />
+      <div className={style['field']}>
+        <div className={style['logo-field']}>
+          <img src={LogoImage.src} alt="" className={style['logo']} />
+          <p className={style['text']}>Cook Pro</p>
+        </div>
+        <img src={userImage} alt="" className={style['image']} />
+      </div>
+      <FormTextField
+        value={value}
+        placeholder="料理名で検索"
+        onChange={onChange}
+      />
     </div>
   );
 };
