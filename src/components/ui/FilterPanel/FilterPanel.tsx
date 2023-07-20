257cf6983cb655c15dbc682bafa8b0ce48bc185d
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-20 12:28:35 +0900

    feat: テンプレート作成

---
+++ src/components/ui/FilterPanel/FilterPanel.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { FC } from 'react';
+
+import style from './index.module.scss';
+
+type Props = {};
+
+export const FilterPanel: FC<Props> = ({}: Props) => {
+  return <div className={style['filter-panel-component']}></div>;
+};
