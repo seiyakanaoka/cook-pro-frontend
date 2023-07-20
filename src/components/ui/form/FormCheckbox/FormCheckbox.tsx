a8e5a0de80807ade6531a0ee2fdb42c8fc2096c2
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-20 15:08:10 +0900

    feat: チェックボックスのスタイルを調整

--- src/components/ui/form/FormCheckbox/FormCheckbox.tsx
+++ src/components/ui/form/FormCheckbox/FormCheckbox.tsx
@@ -12,8 +12,10 @@ type Props = { isCheck: boolean; text: string; onClick: () => void };
 export const FormCheckbox: FC<Props> = ({ isCheck, text, onClick }: Props) => {
   return (
     <div className={style['form-checkbox-component']} onClick={onClick}>
-      <div className={clsx(style['icon'], isCheck && style['-checked'])}>
-        <CheckIcon />
+      <div className={clsx(style['field'], isCheck && style['-checked'])}>
+        <div className={style['icon']}>
+          <CheckIcon />
+        </div>
       </div>
       <p className={style['text']}>{text}</p>
     </div>
