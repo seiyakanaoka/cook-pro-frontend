4fe78d5ecbd2d07d30d7f314184562550459c836
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-20 15:03:31 +0900

    feat: パネル部分とボタンを追加

--- src/components/ui/FilterPanel/FilterPanel.tsx
+++ src/components/ui/FilterPanel/FilterPanel.tsx
@@ -2,10 +2,39 @@
 
 import { FC } from 'react';
 
+import { Button } from '../Button';
+import { FormCheckbox } from '../form/FormCheckbox';
+
 import style from './index.module.scss';
 
-type Props = {};
+type Props = {
+  items: {
+    text: string;
+    isCheck: boolean;
+  }[];
+  onClick: (id: string) => void;
+  onClose: () => void;
+};
 
-export const FilterPanel: FC<Props> = ({}: Props) => {
-  return <div className={style['filter-panel-component']}></div>;
+export const FilterPanel: FC<Props> = ({ items, onClick, onClose }: Props) => {
+  return (
+    <div className={style['filter-panel-component']}>
+      <div className={style['panel-field']}>
+        <p className={style['title']}>カテゴリフィルター</p>
+        <ul className={style['list']}>
+          {items.map((item) => (
+            <FormCheckbox
+              key={item.text}
+              text={item.text}
+              isCheck={item.isCheck}
+              onClick={() => onClick(item.text)}
+            />
+          ))}
+        </ul>
+        <div className={style['action']}>
+          <Button text="閉じる" color="blue" onClick={onClose} />
+        </div>
+      </div>
+    </div>
+  );
 };
