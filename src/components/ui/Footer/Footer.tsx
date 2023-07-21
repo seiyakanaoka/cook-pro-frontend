eda8fded6d355bdb8d6b4db25cc98adebd07fdaf
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-21 12:13:43 +0900

    feat: 推した時に動きをつけれるようにする

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { FC } from 'react';
+import clsx from 'clsx';
+import { FC, useState } from 'react';
 
 import HeartIcon from '@/assets/icons/heart.svg';
 import HomeIcon from '@/assets/icons/home.svg';
@@ -11,17 +12,38 @@ import style from './index.module.scss';
 type Props = {};
 
 export const Footer: FC<Props> = ({}: Props) => {
+  const [select, setSelect] = useState<'home' | 'heart' | 'human'>();
+
+  const selectedIcon = (item: 'home' | 'heart' | 'human') => {
+    setSelect(item);
+  };
+
   return (
     <div className={style['footer-component']}>
-      <div className={style['icon']}>
+      <button
+        onClick={() => selectedIcon('home')}
+        className={clsx(style['icon'], select === 'home' && style['-selected'])}
+      >
         <HomeIcon />
-      </div>
-      <div className={style['icon']}>
+      </button>
+      <button
+        onClick={() => selectedIcon('heart')}
+        className={clsx(
+          style['icon'],
+          select === 'heart' && style['-selected']
+        )}
+      >
         <HeartIcon />
-      </div>
-      <div className={style['icon']}>
+      </button>
+      <button
+        onClick={() => selectedIcon('human')}
+        className={clsx(
+          style['icon'],
+          select === 'human' && style['-selected']
+        )}
+      >
         <HumanIcon />
-      </div>
+      </button>
     </div>
   );
 };
