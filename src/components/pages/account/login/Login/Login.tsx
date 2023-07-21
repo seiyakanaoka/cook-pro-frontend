bc2f6866a186914fd335ad09d8c833178809ac73
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-21 23:21:02 +0900

    feat: 画面にログイン機能を埋め込み

--- src/components/pages/account/login/Login/Login.tsx
+++ src/components/pages/account/login/Login/Login.tsx
@@ -1,19 +1,13 @@
 'use client';
 
-import {
-  CognitoUser,
-  AuthenticationDetails,
-  CognitoUserPool,
-} from 'amazon-cognito-identity-js';
 import { useRouter } from 'next/navigation';
-import { setCookie } from 'nookies';
 import { FC } from 'react';
 
 import { Button } from '@/components/ui/Button';
 import { FormText } from '@/components/ui/form/FormText';
 import { BUTTON_COLOR } from '@/constants/button';
-import { ID_TOKEN_KEY } from '@/constants/cookie';
 import { LOGIN_FORM_VALUES } from '@/constants/validation/login';
+import { useAuth } from '@/hooks/useAuth';
 import { useFormText } from '@/hooks/useFormText';
 import { LoginFormValues } from '@/types/login';
 
@@ -24,47 +18,18 @@ type Props = {};
 export const Login: FC<Props> = ({}: Props) => {
   const { push } = useRouter();
 
+  const { login } = useAuth();
+
   const { fieldValue, fieldState, onChange } = useFormText<LoginFormValues>({
     defaultValues: LOGIN_FORM_VALUES,
   });
 
   const handleLogin = () => {
-    const poolData = {
-      UserPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID ?? '', // CognitoのユーザープールIDに適切な値を設定
-      ClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID ?? '', // CognitoのクライアントIDに適切な値を設定
-    };
+    const userName = fieldValue.userName || fieldValue.email || '';
 
-    const authenticationData = {
-      Username: fieldValue.userName || fieldValue.email || '',
-      Password: fieldValue.password,
-    };
+    const password = fieldValue.password;
 
-    const authenticationDetails = new AuthenticationDetails(authenticationData);
-
-    const userPool = new CognitoUserPool(poolData);
-
-    const userData = {
-      // Eメールも渡せる
-      Username: fieldValue.userName || fieldValue.email || '',
-      Pool: userPool,
-    };
-
-    const cognitoUser = new CognitoUser(userData);
-
-    cognitoUser.authenticateUser(authenticationDetails, {
-      onSuccess: (result) => {
-        const idToken = result.getIdToken().getJwtToken();
-        setCookie(null, ID_TOKEN_KEY, idToken);
-        push('/');
-      },
-      onFailure: (error) => {
-        alert('Authentication error');
-        console.error('error : ', error);
-      },
-      newPasswordRequired: () => {
-        // TODO: 再パスワード入力させる
-      },
-    });
+    login(userName, password, () => push('/'));
   };
 
   return (
