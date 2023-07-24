379c54a5b57b2a59af252fe318da8a3cd5b40beb
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-24 11:53:10 +0900

    feat: ログアウトアイコンを設置

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -2,6 +2,7 @@
 
 import { ChangeEventHandler, FC } from 'react';
 
+import LogoutIcon from '@/assets/icons/logout.svg';
 import LogoImage from 'public/twitter_profile_image.png';
 
 import { FormSearch } from '../form/FormSearch';
@@ -9,26 +10,23 @@ import { FormSearch } from '../form/FormSearch';
 import style from './index.module.scss';
 
 type Props = {
-  userImage: string;
   value: string;
   onSearch: ChangeEventHandler<HTMLInputElement>;
   onClear: () => void;
 };
 
-export const Header: FC<Props> = ({
-  userImage,
-  value,
-  onSearch,
-  onClear,
-}: Props) => {
+export const Header: FC<Props> = ({ value, onSearch, onClear }: Props) => {
+  const handleLogout = () => {};
+
   return (
     <div className={style['header-component']}>
-      <div className={style['field']}>
-        <div className={style['logo-field']}>
+      <div className={style['header-top']}>
+        <div className={style['field']}>
           <img src={LogoImage.src} alt="" className={style['logo']} />
-          <p className={style['text']}>Cook Pro</p>
         </div>
-        <img src={userImage} alt="" className={style['image']} />
+        <div className={style['icon']} onClick={handleLogout}>
+          <LogoutIcon />
+        </div>
       </div>
       <FormSearch
         value={value}
