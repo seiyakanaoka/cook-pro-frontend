d16dc8e4a38cf0e2ad76aa0e09484145deac87ab
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-24 21:05:51 +0900

    feat: カラー変更できるようにした

--- src/components/ui/SnackBar/SnackBar.tsx
+++ src/components/ui/SnackBar/SnackBar.tsx
@@ -1,14 +1,33 @@
 'use client';
 
+import clsx from 'clsx';
 import { FC, MouseEventHandler } from 'react';
 
 import style from './index.module.scss';
 
-type Props = { text: string; onClick: MouseEventHandler<HTMLButtonElement> };
+const SNACKBAR_STATUS = {
+  NORMAL: 'normal',
+  ABNORMAL: 'abnormal',
+} as const;
 
-export const Snackbar: FC<Props> = ({ text, onClick }: Props) => {
+type SnackbarStatus = (typeof SNACKBAR_STATUS)[keyof typeof SNACKBAR_STATUS];
+
+type Props = {
+  text: string;
+  color?: SnackbarStatus;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+export const Snackbar: FC<Props> = ({
+  text,
+  color = SNACKBAR_STATUS.NORMAL,
+  onClick,
+}: Props) => {
   return (
-    <button className={style['snackbar-component']} onClick={onClick}>
+    <button
+      className={clsx(style['snackbar-component'], style[`-${color}`])}
+      onClick={onClick}
+    >
       {text}
     </button>
   );
