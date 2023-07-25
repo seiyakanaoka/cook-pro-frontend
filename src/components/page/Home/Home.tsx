ef8e9d3dcd7648c8a5c2c17ac75fa7e3529cd1d8
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-25 10:53:26 +0900

    feat: 邪魔なエラーを削除

--- src/components/page/Home/Home.tsx
+++ src/components/page/Home/Home.tsx
@@ -6,7 +6,6 @@ import { FC, Fragment, useEffect, useState } from 'react';
 import { DishItem } from '@/components/model/dish/DishItem';
 import { FilterAction } from '@/components/ui/filter/FilterAction';
 import { FilterPanel } from '@/components/ui/filter/FilterPanel';
-import { axiosClient } from '@/utils/axios';
 import FoodImage from 'public/food-1.png';
 
 import style from './index.module.scss';
@@ -54,11 +53,11 @@ const dishes = {
 
 export const Home: FC = () => {
   useEffect(() => {
-    const aaa = async () => {
-      const response = await axiosClient.get('/category');
-      console.log('response : ', response.data);
-    };
-    aaa();
+    // const aaa = async () => {
+    //   const response = await axiosClient.get('/category');
+    //   console.log('response : ', response.data);
+    // };
+    // aaa();
   }, []);
 
   const [items, setItems] = useState([
