294c83220fa2514da337512425080aecbd20f699
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-26 18:13:37 +0900

    feat: api urlをapi.tsに追加

--- src/constants/api/api.ts
+++ src/constants/api/api.ts
@@ -1,7 +1,12 @@
+import { API_URL_DISHES, API_URL_DISHES_SEARCH } from './dish';
 import { API_URL_USER_SIGN_UP } from './user';
 
 export const API_URL = {
   USER: {
     SIGN_UP: API_URL_USER_SIGN_UP,
+  },
+  DISH: {
+    DISHES: API_URL_DISHES,
+    DISHES_SEARCH: API_URL_DISHES_SEARCH,
   },
 } as const;
