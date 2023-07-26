a554279664cd036aca60208bf51fad3d1c12ace1
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-26 20:11:42 +0900

    style: カテゴリーの表示定数を作成

---
+++ src/constants/category.ts
@@ -0,0 +1,11 @@
+export const CATEGORY = {
+  JAPAN_FOOD: '和食',
+  WESTERN_FOOD: '洋食',
+  CHINESE_FOOD: '中華',
+  MEAT_DISH: '肉料理',
+  FISH_DISH: '魚料理',
+  NOODLE: '麺',
+  RICE: '米',
+  SALAD: 'サラダ',
+  WITH_ALCOHOL: 'お酒に合う',
+} as const;
