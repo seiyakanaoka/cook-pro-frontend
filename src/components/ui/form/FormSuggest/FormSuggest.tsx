300c2b9693a31781d2ce0ae09229e2ff3a22963c
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-27 15:41:33 +0900

    feat: マークあっぷ

--- src/components/ui/form/FormSuggest/FormSuggest.tsx
+++ src/components/ui/form/FormSuggest/FormSuggest.tsx
@@ -1,11 +1,52 @@
 'use client';
 
-import { FC } from 'react';
+import { ChangeEventHandler, FC } from 'react';
+
+import { FORM_TEXT_FIELD_TYPE } from '@/constants/form';
+
+import { FormSearch } from '../FormSearch';
 
 import style from './index.module.scss';
 
-type Props = {};
+type Props = {
+  items: { id: string; name: string }[];
+  value: string;
+  placeholder?: string;
+  onSearch: ChangeEventHandler<HTMLInputElement>;
+  onClear: () => void;
+};
 
-export const FormSuggest: FC<Props> = ({}: Props) => {
-  return <div className={style['form-suggest-component']}></div>;
+export const FormSuggest: FC<Props> = ({
+  items,
+  value,
+  placeholder = '入力してください',
+  onSearch,
+  onClear,
+}: Props) => {
+  return (
+    <div className={style['form-suggest-component']}>
+      <FormSearch
+        value={value}
+        type={FORM_TEXT_FIELD_TYPE.TEXT}
+        placeholder={placeholder}
+        onSearch={onSearch}
+        onClear={onClear}
+      />
+      {!!value && (
+        <div className={style['panel']}>
+          {!items.length ? (
+            <div>結果がありません</div>
+          ) : (
+            <ul className={style['select-panel']}>
+              {items.map((item) => (
+                <li key={item.id} className={style['item']}>
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
