20d2c537307b5fe0e30a9da7eb517a67b6fe7e14
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-27 15:33:05 +0900

    feat: テンプレート作成

---
+++ src/components/ui/form/FormSuggest/FormSuggest.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { FC } from 'react';
+
+import style from './index.module.scss';
+
+type Props = {};
+
+export const FormSuggest: FC<Props> = ({}: Props) => {
+  return <div className={style['form-suggest-component']}></div>;
+};
