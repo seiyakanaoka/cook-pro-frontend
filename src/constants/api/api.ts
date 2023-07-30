7aaffe085b0d29b5153c9858b11ecae4c0ac3df5
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-30 20:57:06 +0900

    feat: apiのurlを作成

--- src/constants/api/api.ts
+++ src/constants/api/api.ts
@@ -1,9 +1,10 @@
 import { API_URL_CATEGORIES } from './category';
 import { API_URL_DISHES, API_URL_DISHES_SEARCH } from './dish';
-import { API_URL_USER_SIGN_UP } from './user';
+import { API_URL_USER, API_URL_USER_SIGN_UP } from './user';
 
 export const API_URL = {
   USER: {
+    USER: API_URL_USER,
     SIGN_UP: API_URL_USER_SIGN_UP,
   },
   DISH: {
