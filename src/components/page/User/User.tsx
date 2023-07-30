0b4e151711b1c52958f75d29890c6f1eed56cd6b
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-30 21:14:38 +0900

    feat: apiレスポンスを画面に反映する

--- src/components/page/User/User.tsx
+++ src/components/page/User/User.tsx
@@ -1,11 +1,26 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { UserDetail } from '@/components/section/user/UserDetail';
+import { useUser } from '@/hooks/api/user/useUser';
+import { UserResponse } from '@/types/codegen/user/UserResponse';
 
 type Props = {};
 
 export const User: FC<Props> = ({}: Props) => {
-  return <UserDetail />;
+  const { getUser: _getUser } = useUser();
+
+  const [user, setUser] = useState<UserResponse | undefined>();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await _getUser();
+      setUser(response);
+    };
+
+    getUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <UserDetail user={user} />;
 };
--- src/components/section/user/UserDetail/UserDetail.tsx
+++ src/components/section/user/UserDetail/UserDetail.tsx
@@ -4,13 +4,15 @@ import { FC } from 'react';
 
 import { Button } from '@/components/ui/Button';
 import { PAGE_URL } from '@/constants/route';
-import LogoImage from 'public/twitter_profile_image.png';
+import { UserResponse } from '@/types/codegen/user/UserResponse';
 
 import style from './index.module.scss';
 
-type Props = {};
+type Props = {
+  user: UserResponse | undefined;
+};
 
-export const UserDetail: FC<Props> = ({}: Props) => {
+export const UserDetail: FC<Props> = ({ user }: Props) => {
   const { push } = useRouter();
 
   const navigateToEdit = () => {
@@ -21,14 +23,16 @@ export const UserDetail: FC<Props> = ({}: Props) => {
     <div className={style['user-detail-component']}>
       <div className={style['top']}>
         <div className={style['image-field']}>
-          <img src={LogoImage.src} alt="" className={style['image']} />
+          <img src={user?.image} alt="" className={style['image']} />
         </div>
         <div className={style['user-info']}>
-          <p className={style['nickname']}>Seiya</p>
+          <p className={style['nickname']}>
+            {user?.displayUserName ?? user?.name}
+          </p>
           <div className={style['detail']}>
-            <p className={style['name']}>kanaokaseiya</p>
-            <p className={style['email']}>hoge@hoge.com</p>
-            <p className={style['telephone']}>000-0000-0000</p>
+            <p className={style['name']}>{user?.name}</p>
+            <p className={style['email']}>{user?.email}</p>
+            <p className={style['telephone']}>{user?.telNumber}</p>
           </div>
         </div>
       </div>
