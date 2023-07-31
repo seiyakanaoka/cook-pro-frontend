0c9fc8be7293934b2cfd15b8fd65495130cd2ac6
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-07-31 11:42:15 +0900

    feat: マークアップ

--- src/components/section/dish/DishDetail/DishDetail.tsx
+++ src/components/section/dish/DishDetail/DishDetail.tsx
@@ -1,11 +1,56 @@
 'use client';
 
+import clsx from 'clsx';
 import { FC } from 'react';
+
+import { DishCategory } from '@/components/model/dish/DishCategory';
+import { DishTime } from '@/components/model/dish/DishTime';
+import { CategoryResponse } from '@/types/codegen/category/CategoryResponse';
+import DishImage from 'public/food-1.png';
 
 import style from './index.module.scss';
 
 type Props = {};
 
 export const DishDetail: FC<Props> = ({}: Props) => {
-  return <div className={style['dish-detail-component']}></div>;
+  const title = '【マネしてほしい料理！！】\n最高のメインディッシュ';
+
+  const titles = title.split('\n');
+
+  return (
+    <div className={style['dish-detail-component']}>
+      <div className={style['top']}>
+        <img src={DishImage.src} alt="" className={style['image']} />
+        <div className={style['image-dot']}>
+          <div className={style['dot']}></div>
+          <div className={clsx(style['dot'], style['-selected'])}></div>
+          <div className={style['dot']}></div>
+        </div>
+      </div>
+      <div className={style['bottom']}>
+        <div className={style['title-field']}>
+          {titles.map((text, i) => (
+            <p className={style['title']} key={`${i}-${text}`}>
+              {text}
+            </p>
+          ))}
+        </div>
+        <ul className={style['category-field']}>
+          <li className={style['category']}>
+            <DishCategory category={CategoryResponse.JAPAN_FOOD} />
+          </li>
+          <li className={style['category']}>
+            <DishCategory category={CategoryResponse.MEAT_DISH} />
+          </li>
+          <li className={style['category']}>
+            <DishCategory category={CategoryResponse.FISH_DISH} />
+          </li>
+          <li className={style['category']}>
+            <DishCategory category={CategoryResponse.SALAD} />
+          </li>
+        </ul>
+        <DishTime time="20" />
+      </div>
+    </div>
+  );
 };
