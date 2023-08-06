df5bae979c5c52b22e1f790affab92d483f2aabb
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-06 18:24:27 +0900

    feat: マークアップ

--- src/components/ui/form/formSelect/FormSelect/FormSelect.tsx
+++ src/components/ui/form/formSelect/FormSelect/FormSelect.tsx
@@ -1,9 +1,48 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+
+import { FormSelectPlate } from '../FormSelectPlate';
 
 import style from './index.module.scss';
 
-type Props = {};
+type Props = {
+  items: { id: string; name: string }[];
+  selectedValue: string | undefined;
+  placeholder?: string;
+  onClick: (id: string) => void;
+};
 
-export const FormSelect: FC<Props> = ({}: Props) => {
-  return <div className={style['form-select-component']}></div>;
+export const FormSelect: FC<Props> = ({
+  items,
+  selectedValue,
+  placeholder = '入力してください',
+  onClick,
+}: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const onOpen = () => {
+    setIsOpen(true);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleClick = (id: string) => {
+    onClick(id);
+    onClose();
+  };
+
+  return (
+    <div className={style['form-select-component']}>
+      <div className={style['field']} onClick={onOpen}>
+        {selectedValue || placeholder}
+        <span className={style['icon']}></span>
+      </div>
+      {isOpen && (
+        <div className={style['panel']}>
+          <FormSelectPlate items={items} onClick={handleClick} />
+        </div>
+      )}
+    </div>
+  );
 };
