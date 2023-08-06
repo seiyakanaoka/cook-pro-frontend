86384ec8f3cc75690a48850661025a307cfa6706
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-06 17:21:02 +0900

    feat: テンプレート作成

---
+++ src/components/ui/form/formSelect/FormSelectPlate/FormSelectPlate.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import style from './index.module.scss';
+
+type Props = {};
+
+export const FormSelectPlate: FC<Props> = ({}: Props) => {
+  return <div className={style['form-select-plate-component']}></div>;
+};
