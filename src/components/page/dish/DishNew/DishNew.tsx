95be56094138e16f6fa300b42b7f3f3cc614f378
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-07 10:36:39 +0900

    feat:form-selectにタイトル表示を追加する

--- src/components/page/dish/DishNew/DishNew.tsx
+++ src/components/page/dish/DishNew/DishNew.tsx
@@ -1,9 +1,23 @@
 import { FC } from 'react';
 
+import { FormSelect } from '@/components/ui/form/formSelect/FormSelect';
+import { FormText } from '@/components/ui/form/FormText';
+
 import style from './index.module.scss';
 
 type Props = {};
 
 export const DishNew: FC<Props> = ({}: Props) => {
-  return <div className={style['dish-new-component']}>aaa</div>;
+  return (
+    <div className={style['dish-new-component']}>
+      <h1 className={style['title']}>料理新規登録</h1>
+      <FormText title="料理名" value="" onChange={() => {}} />
+      <FormSelect
+        title="所要時間"
+        items={[]}
+        selectedValue=""
+        onClick={() => {}}
+      />
+    </div>
+  );
 };
