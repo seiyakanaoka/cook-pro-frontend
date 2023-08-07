f5da4a47200ff50d027fd9253fba79b7cf6d798f
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-07 21:35:01 +0900

    feat: api hooksを作成

--- src/hooks/api/dish/useDishRequest.ts
+++ src/hooks/api/dish/useDishRequest.ts
@@ -1,22 +1,34 @@
-import { postDish, deleteDish as _deleteDish } from '@/api/dish';
+import { postDish, deleteDish as _deleteDish, putDish } from '@/api/dish';
 import { API_URL } from '@/constants/api/api';
 import { PostDishRequest } from '@/types/codegen/dish/PostDishRequest';
 import { PostDishResponse } from '@/types/codegen/dish/PostDishResponse';
+import { PutDishRequest } from '@/types/codegen/dish/PutDishRequest';
+import { PutDishResponse } from '@/types/codegen/dish/PutDishResponse';
 
 type UseDishRequest = {
   createDish: (requestBody: PostDishRequest) => Promise<PostDishResponse>;
+  editDish: (requestBody: PutDishRequest) => Promise<PutDishResponse>;
   deleteDish: (dishId: string) => Promise<void>;
 };
 
 export const useDishRequest = (): UseDishRequest => {
-  const createDish = async (requestBody: PostDishRequest) => {
+  const createDish = async (
+    requestBody: PostDishRequest
+  ): Promise<PostDishResponse> => {
     const response = await postDish(API_URL.DISH.POST_DISH, requestBody);
     return response;
   };
 
-  const deleteDish = async (dishId: string) => {
+  const editDish = async (
+    requestBody: PutDishRequest
+  ): Promise<PutDishResponse> => {
+    const response = await putDish(API_URL.DISH.POST_DISH, requestBody);
+    return response;
+  };
+
+  const deleteDish = async (dishId: string): Promise<void> => {
     await _deleteDish(API_URL.DISH.DISH.replace(':dishId', dishId));
   };
 
-  return { createDish, deleteDish };
+  return { createDish, editDish, deleteDish };
 };
