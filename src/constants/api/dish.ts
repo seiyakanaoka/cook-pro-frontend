cb8ff1405044bbb13203cf31c1bd5d1fb9c2fc7c
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-07 21:32:14 +0900

    feat: apiのurlを作成

--- src/constants/api/dish.ts
+++ src/constants/api/dish.ts
@@ -7,3 +7,5 @@ export const API_URL_DISHES_SEARCH = '/dish/search' as const;
 export const API_URL_DISH_MATERIALS = '/dish/:dishId/material' as const;
 
 export const API_URL_POST_DISH = '/dish' as const;
+
+export const API_URL_PUT_DISH = '/dish/:dishId' as const;
