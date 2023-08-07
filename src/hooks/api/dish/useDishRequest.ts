c9d877db83f55d98bacef1064465716a6332f993
kanaokaseiya <ttf.1998.seiya@gmail.com>
2023-08-07 18:00:12 +0900

    feat: api hooksを作成

--- src/hooks/api/dish/useDishRequest.ts
+++ src/hooks/api/dish/useDishRequest.ts
@@ -1,10 +1,11 @@
-import { postDish } from '@/api/dish';
+import { postDish, deleteDish as _deleteDish } from '@/api/dish';
 import { API_URL } from '@/constants/api/api';
 import { PostDishRequest } from '@/types/codegen/dish/PostDishRequest';
 import { PostDishResponse } from '@/types/codegen/dish/PostDishResponse';
 
 type UseDishRequest = {
   createDish: (requestBody: PostDishRequest) => Promise<PostDishResponse>;
+  deleteDish: (dishId: string) => Promise<void>;
 };
 
 export const useDishRequest = (): UseDishRequest => {
@@ -13,5 +14,9 @@ export const useDishRequest = (): UseDishRequest => {
     return response;
   };
 
-  return { createDish };
+  const deleteDish = async (dishId: string) => {
+    await _deleteDish(API_URL.DISH.DISH.replace(':dishId', dishId));
+  };
+
+  return { createDish, deleteDish };
 };
